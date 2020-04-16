16b9f6edcf0c10c4912e9f2e2a0fdd0575e89695
AlexSpart <alex1_spart@yahoo.gr>
2020-04-16 18:08:06 +0200

    cleanup

--- unix_sock_cpp/client.cpp
+++ unix_sock_cpp/client.cpp
@@ -10,39 +10,15 @@
 
 #include "quadkey.hpp"
 
-
-//#include "my_class.h"
-
 using namespace std; 
-//using namespace N;
-//using namespace tiles;
 
-#define DATA "Half a league, half a league . . ."
+
+
 #define NAME "/tmp/quad_socket"
 
 
-//void my_class::do_something()
-//{
-//    cout << "Doing something!" << endl;
-//}
-
 main(int argc, char** argv)
  {
-     bool alex;
-     string str;
-     int a;
-     int b; 
-     int c;
-     double d;
-     //my_class mc;
-    // mc.do_something();
-     TileSystem ts;
-     ts.do_something();
-     ts.LatLongToPixelXY(40,7,3,a,b);
-     //ts.TileXYToQuadKey(40,7,2,true);
-     //str = ts.TileXYToQuadKey(40,7,2,alex);
-
-
      int sock;
      struct sockaddr_un server;
      char buf[1024];
